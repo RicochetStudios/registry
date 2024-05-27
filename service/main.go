ab13b9f042f15a9be53c9c892e5ab32ac5495ef3
kiweezi <46600706+kiweezi@users.noreply.github.com>
2024-05-27 08:51:27 +0000

    Update main lifecycle

--- service/main.go
+++ service/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	sdk "agones.dev/agones/sdks/go"
 )
@@ -11,7 +14,8 @@ import (
 func Run(wrapper ServerWrapper) {
 	// TODO: Allow extra arguments here.
 
-	ctx := context.Background()
+	// Create a context that is cancelled when an interrupt or term signal is received.
+	ctx := newSignalContext()
 
 	// Connect to agones SDK.
 	fmt.Println("Connecting to Agones with the SDK")
@@ -20,9 +24,9 @@ func Run(wrapper ServerWrapper) {
 		log.Fatalf("Could not connect to Agones sdk: %v", err)
 	}
 
-	// // Start health check.
-	// fmt.Println("Starting health check")
-	// go DoHealth(wrapper, s, ctx)
+	// Start health check.
+	fmt.Println("Starting health check")
+	go DoHealth(wrapper, s, ctx)
 
 	// Start the server.
 	fmt.Println("Starting the server")
@@ -33,6 +37,8 @@ func Run(wrapper ServerWrapper) {
 	// Stop the server when the program ends.
 	defer wrapper.Stop()
 
+	// If an interupt or term signal is received, stop the server.
+
 	// Wait for the server to be ready.
 	fmt.Println("Waiting for the server to be ready")
 	if err = wrapper.Wait(); err != nil {
@@ -40,9 +46,21 @@ func Run(wrapper ServerWrapper) {
 	}
 	s.Ready()
 
-	// // Serve to clients.
-	// fmt.Println("Serving the server")
-	// if err = wrapper.Serve(ctx); err != nil {
-	// 	log.Fatalf("Server failed to serve: %v", err)
-	// }
+	// Serve to clients.
+	fmt.Println("Serving the server")
+	if err = wrapper.Serve(ctx); err != nil {
+		log.Fatalf("Server failed to serve: %v", err)
+	}
+}
+
+// newSignalContext creates a new context that is cancelled when an interrupt or term signal is received.
+func newSignalContext() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+		<-sig
+		cancel()
+	}()
+	return ctx
 }
